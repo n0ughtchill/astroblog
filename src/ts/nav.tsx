bfefb4fc084cda28c0a2d372573f485ef86c21b5
n0ughtchill <brian.stempien@protonmail.com>
2023-02-26 07:43:05 -0600

    update

---
+++ src/ts/nav.tsx
@@ -0,0 +1,9 @@
+const navLinks = document.querySelectorAll("[data-navLink]");
+
+navLinks.forEach((link) => {
+    if(link.getAttribute("href") === window.location.pathname) {
+        link.setAttribute("aria-current", "page");
+    }
+})
+
+export default navLinks;
